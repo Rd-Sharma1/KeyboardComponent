a994a254e60050b8fa2168ba514c99068378bd9b
Rd-Sharma1 <ridamgreat@gmail.com>
2026-06-09 21:56:34 +0530

    layout complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import Key from './Key'
 
  type KeyRow = Record<string, string>
-    const keyChars : KeyRow[]  = [
-      {"esc": "medium", "f1": "letter", "f2": "letter", "f3": "letter", "f4": "letter", "f5": "letter", "f6": "letter", "f7": "letter", "f8": "letter", "f9": "letter", "f10": "letter", "f11": "letter", "f12": "letter", "empty": "letter"},
 
-      {"~" : "letter", "1": "letter", "2": "letter", "3": "letter", "4": "letter", "5": "letter", "6": "letter", "7": "letter", "8": "letter", "9": "letter", "0": "letter", "-": "letter", "=": "letter", "backspace": "lg",},
+    const keyChars : KeyRow[]  = [
+      {"esc": "md", "f1": "letter", "f2": "letter", "f3": "letter", "f4": "letter", "f5": "letter", "f6": "letter", "f7": "letter", "f8": "letter", "f9": "letter", "f10": "letter", "f11": "letter", "f12": "letter", "empty": "letter"},
+
+      {"~" : "letter", "1": "letter", "2": "letter", "3": "letter", "4": "letter", "5": "letter", "6": "letter", "7": "letter", "8": "letter", "9": "letter", "0": "letter", "-": "letter", "=": "letter", "delete": "md",},
 
       {"tab": "md","Q": "letter", "W": "letter", "E": "letter", "R": "letter", "T": "letter", "Y": "letter", "U": "letter", "I": "letter", "O": "letter", "P": "letter", "[": "letter", "]": "letter", "\\": "letter",},
 
@@ -12,23 +13,42 @@ import Key from './Key'
 
       {"shift": "xlg", "Z": "letter", "X": "letter", "C": "letter", "V": "letter", "B": "letter", "N": "letter", "M": "letter", ",": "letter", ".": "letter", "/": "letter","shift2": "xlg",},
 
-     { "ctrl": "half", "win": "letter", "alt": "letter", "space": "space", "altGr": "letter", "fn": "letter", "menu": "letter", "ctrl2": "letter"},
+     { "ctrl": "half", "fn": "letter", "win": "letter", "alt": "letter", "space": "space", "altGr": "letter", "ctrl2": "letter", "←": "letter", "↑|↓": "verArr", "→": "letter",},
     ]
 
+console.log(keyChars);
+
+function KeyRowComponent ({row} : {row: KeyRow}) {
+  // console.log(row);
+
+  return (
+    <div className='w-full flex justify-start items-center gap-x-0.75 my-0.5'>
+        {Object.entries(row).map(([k, t])=>(<Key char={k} type={t} />))}
+    </div>
+  )
+}
 
 function App() {
 
   return (
     <>
-    <div className=' bg-neutral-900 h-screen flex justify-center items-center'>
+    <div className=' bg-neutral-900 h-screen flex justify-center items-center box-border'>
 
-      <div className='w-200 p-2 bg-neutral-300 border-2 border-neutral-950 rounded-3xl flex flex-wrap gap-1 justify-center items-center'>
-        {keyChars.map((row) => (
+      {/* <div className='w-200 p-2 bg-neutral-300 border-2 border-neutral-950 rounded-3xl flex flex-wrap gap-1 justify-center items-center'> */}
+        {/* {keyChars.map((row) => (
          Object.entries(row).map(([char, type]) => 
          <Key key={char} char={char} type={type}/>
         )
       ))
-      }
+        } */ }
+        {/* <div className='text-4xl text-sky-200 text-center'>Ice Cream</div> */}
+              <div className='w-204 p-2 bg-neutral-300 border-2 border-neutral-950 rounded-3xl flex flex-col justify-center items-center'>
+
+        {
+          keyChars.map((row, index) => (
+            <KeyRowComponent key={index} row={row} />
+          ))
+        }
       </div>
     </div>
 
--- src/Key.tsx
+++ src/Key.tsx
@@ -1,17 +1,57 @@
 
-function Key({char , type}: {char: string, type: string}) {
+function Key({ char, type }: { char: string; type: string }) {
+  const widthSpecs: Record<string, number> = {
+    letter: 26, 
+    md: 40,     
+    lg: 46,     
+    xlg: 60,    
+    half: 40,   
+    space: 132, 
+  };
 
-  return (
+  const calculatedWidth = (widthSpecs[type] || widthSpecs.letter)*2;
+
+    const isStacked = char.includes('|');
+    const subKeys = isStacked ? char.split('|') : [char];
+  
+     return (
     <>
-        <button className = {`w-${type} h-6 border-2 rounded-xl bg-neutral-200 flex justify-center items-center  border-neutral-400 p-6`}>
-              <div className="w-full text-xs font-medium text-neutral-900">
-            {char}
-        </div>
+  
+    <button
+            style={{ width: `${calculatedWidth}px`}}
 
-        </button>
-      
+      className="
+        h-12
+      relative
+        flex justify-center items-center 
+        rounded-xl border border-neutral-400 
+        bg-neutral-200 
+        transition-all duration-30
+        box-border shrink-0
+        inset-shadow-xs inset-shadow-neutral-50
+        shadow-2xs shadow-neutral-400 
+        flex-col overflow-hidden
+        hover: cursor-pointer 
+        transform active:scale-98 active:inset-shadow-none
+        active:shadow-none
+        "
+        >
+  
+        {
+            isStacked ? 
+            subKeys.map((arrKey, idx)=>(
+                <div key={idx} className="w-full h-6 text-neutral-900 text-center text-xs border rounded-t-sm border-neutral-50 ">{arrKey}</div>
+            ))
+            :
+            <div className="w-full text-[10px] font-medium text-neutral-900 lowercase text-center">
+                {char === "empty" ? "" : char}
+            </div>
+        }
+        <div className='absolute top-px inset-x-0 w-11/12 h-px bg-linear-to-r from-transparent via-neutral-50 to-transparent'>
+    </div>
+    </button>
     </>
-  )
-}
+  );
+   }
 
-export default Key
+export default Key;
